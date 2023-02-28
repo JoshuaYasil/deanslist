24b7e9a5ebe481fc4c9725df2b343f23ad314aa1
JoshuaYasil <xt202001326@wmsu.edu.ph>
2023-02-28 09:49:30 +0800

    Initial commit

---
+++ home.php
@@ -0,0 +1,55 @@
+
+          <h1 class="page-header">Dashboard</h1>
+          <div class="home-content">
+                <div class="overview-boxes">
+                        <div class="box">
+                                <div class="right-side">
+                                    <div class="box-topic">Number of Students</div>
+                                    <div class="number">6969</div>
+                                    <div class="indicator">
+
+                                        <span class="text">As of <?php echo ' ' . date("m-d-Y h:i:s A"); ?></span>
+                                    </div>
+                                </div>
+                            <i class='bx bx-shopping-bag'></i>
+
+                        </div>
+
+                        <div class="box">
+                                    <div class="right-side">
+                                        <div class="box-topic">Faculty</div>
+                                        <div class="number">20</div>
+                                        <div class="indicator">
+
+                                            <span class="text">As of <?php echo ' ' . date("m-d-Y h:i:s A"); ?></span>
+                                        </div>
+                                    </div>
+                                <i class='bx bx-cart-download'></i>
+                        </div>
+
+                        <div class="box">
+                                <div class="right-side">
+                                    <div class="box-topic">Programs</div>
+                                    <div class="number">6</div>
+                                    <div class="indicator">
+
+                                        <span class="text">As of <?php echo ' ' . date("m-d-Y h:i:s A"); ?></span>
+                                    </div>
+                                </div>
+                            <i class='bx bx-coin-stack'></i>
+                        </div>
+
+                        <div class="box">
+                                <div class="right-side">
+                                    <div class="box-topic">Departments</div>
+                                    <div class="number">2</div>
+                                    <div class="indicator">
+
+                                    <span class="text">As of <?php echo ' ' . date("m-d-Y h:i:s A"); ?></span>
+                                    </div>
+                                </div>
+                            <i class='bx bx-bar-chart'></i>
+                        </div>
+
+                </div>
+        </div>
